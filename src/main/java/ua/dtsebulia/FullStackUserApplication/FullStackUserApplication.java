7f372097d015ef3f818c055827afe1aad3a4ce49
d3nnyyy <d3nnn41k@gmail.com>
2023-06-25 12:27:20 +0300

    Initial commit

---
+++ src/main/java/ua/dtsebulia/FullStackUserApplication/FullStackUserApplication.java
@@ -0,0 +1,13 @@
+package ua.dtsebulia.FullStackUserApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackUserApplication.class, args);
+	}
+
+}
